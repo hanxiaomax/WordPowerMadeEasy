41f158a0588c43a90815b0e50b5c893e0ebd4c91
hanxiaomax-mac <hanxiaomax@qq.com>
2017-06-17 00:17:27 +0800

    把思维导图内容显示部分移动到单独的文件

--- docs/static/js/mindmaps.js
+++ docs/static/js/mindmaps.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2014-2016 hanxiaomax@qq.com
  *
  */
-var mind = {
+var mind1 = {
     /* 元数据，定义思维导图的名称、作者、版本等信息 */
     "meta": {
         "name": "How to talk about personality",
@@ -16,6 +16,59 @@ var mind = {
     "data": {
         "id": "root",
         "topic": "How to talk about personality",
+        "children": [{
+            "id": "easy",
+            "topic": "<div data-toggle='popover' data-content='这里是英文翻译'>easy to use</div>",
+            "direction": "left",
+            "expanded": false,
+            "children": [
+                { "id": "easy1", "topic": "Easy to show" },
+                { "id": "easy2", "topic": "<div title='123322bkhjbkjhbjhkbkhjbkjhbkjbjkbkjhjvvjhvjhvjhvjhvjhgvjhvjhvjvj6'>easy to 11e</div>" },
+                { "id": "easy3", "topic": "Easy to store" },
+                { "id": "easy4", "topic": "Easy to embed" }
+            ]
+        }, {
+            "id": "open",
+            "topic": "Open Source",
+            "direction": "right",
+            "expanded": true,
+            "children": [
+                { "id": "open1", "topic": "on GitHub" },
+                { "id": "open2", "topic": "BSD License" }
+            ]
+        }, {
+            "id": "powerful",
+            "topic": "Powerful",
+            "direction": "right",
+            "children": [
+                { "id": "powerful1", "topic": "Base on Javascript" },
+                { "id": "powerful2", "topic": "Base on HTML5" },
+                { "id": "powerful3", "topic": "Depends on you" }
+            ]
+        }, {
+            "id": "other",
+            "topic": "test node",
+            "direction": "left",
+            "children": [
+                { "id": "other1", "topic": "I'm from local variable" },
+                { "id": "other2", "topic": "I can do everything" }
+            ]
+        }]
+    }
+};
+var mind2 = {
+    /* 元数据，定义思维导图的名称、作者、版本等信息 */
+    "meta": {
+        "name": "How to talk about doctors",
+        "author": "lingfeng_ai",
+        "version": "0.2"
+    },
+    /* 数据格式声明 */
+    "format": "node_tree",
+    /* 数据内容 */
+    "data": {
+        "id": "root",
+        "topic": "How to talk about doctors",
         "children": [{
             "id": "easy",
             "topic": "<div data-toggle='popover' data-content='这里是英文翻译'>easy to use</div>",
@@ -74,7 +127,7 @@ var options = {
     },
 };
 var jm = new jsMind(options);
-jm.show(mind);
+
 var options = {
     container: 'jmnodes',
     trigger: 'hover',
